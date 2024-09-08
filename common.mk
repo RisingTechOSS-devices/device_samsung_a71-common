59b7976760109a86fca958abba61f2ca571d22d2
Kelvin Zhang <zhangkelvin@google.com>
2024-09-08 13:32:28 +0530

    a71-common: Allow to access bootable/deprecated-ota targets
    Galaxy A71 is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to a71-common namespace list.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d

--- common.mk
+++ common.mk
@@ -417,6 +417,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.sm6150
 
+# Update
+PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
